2ba041d8375ecacbd760adefd5ce0262cc102ca5
AlejandroPardoS <a.pardos2@uniandes.edu.co>
2024-11-15 16:00:37 -0500

    casi todo hecho

--- ProblemaP2.py
+++ ProblemaP2.py
@@ -45,39 +45,6 @@ def bfs(lista_adyacencias, nodo1, nodo2):
                 
     return None
 
-def edmonds_karp2(lista_adyacencias, nodo1, nodo2):
-    max_flow = 0 
-    while True:
-        parent = bfs(lista_adyacencias, nodo1, nodo2)
-        if parent is None:
-            break 
-
-        path_flow = float("inf")
-        s = nodo2
-
-        while s != nodo1:
-            u = parent[s]
-            #print(u)
-            for v, capacity in lista_adyacencias[str(u)]:
-                if v == s:
-                    path_flow = min(path_flow, capacity)
-            s = parent[s]
-
-        max_flow += path_flow
-
-        v = nodo2
-        while v != nodo1:
-            u = parent[v]
-            for i, (adj_v, capacity) in enumerate(lista_adyacencias[str(u)]):
-                if adj_v == v:
-                    lista_adyacencias[str(u)][i] = (adj_v, capacity - path_flow)
-            for i, (adj_u, capacity) in enumerate(lista_adyacencias[str(v)]):
-                if adj_u == u:
-                    lista_adyacencias[str(v)][i] = (adj_u, capacity + path_flow)
-            v = parent[v]
-
-    return max_flow
-
 def edmonds_karp(lista_adyacencias, nodo1, nodo2):
     max_flow = 0
     modificaciones = []
@@ -113,13 +80,11 @@ def edmonds_karp(lista_adyacencias, nodo1, nodo2):
         #print(modificaciones)
     # Revertir las modificaciones realizadas si hay
     if modificaciones:
+        #print(modificaciones)
         for nodo, i, capacidad_original in modificaciones:
             lista_adyacencias[nodo][i] = ((lista_adyacencias[nodo][i][0], capacidad_original))
 
-    return max_flow#,  lista_adyacencias
-
-#print(edmonds_karp({"s": [("1",float('inf')), ("2",float('inf'))], "t": [], "1": [(3, 2)], "2": [(5, 3)], "3": [(1, 2), (4, 3)], "4": [], "5": [(2, 3), (4, 1), (7, 1)], "6": [(4, 2), ("t", float('inf'))], "7": [(5, 1), ("t", float('inf'))]}, "s", "t"))
-#print(edmonds_karp({"s": [("1",float('inf')), ("2",float('inf'))], "t": [], "1": [(3, 2)], "2": [(5, 3)], "3": [(1, 2), (4, 3)], "4": [(3, 3), (5, 1), (6, 2)], "5": [(2, 3), (4, 1), (7, 1)], "6": [(4, 2), ("t", float('inf'))], "7": [(5, 1), ("t", float('inf'))]}, "s", "t"))
+    return max_flow
 
 def main():
     calculadoras = []
@@ -127,76 +92,65 @@ def main():
     ncasos = int(sys.stdin.readline().strip())
     linea = sys.stdin.readline() 
     for i in range(0, ncasos):
-        #try :
-        numCelulas, dist = linea.split()
-        numCelulas, dist = int(numCelulas), int(dist)
-        celulas = []
-        crearNodo(lista_adyacencias, "i")
-        crearNodo(lista_adyacencias, "f")
-        for i in range(numCelulas):
-            linea = sys.stdin.readline()
-            linea = linea.split()
-            id = (linea[0])
-            x = int(linea[1])
-            y = int(linea[2])
-            tipo = int(linea[3])
-            peptidos = linea[4:]
-            celulas.append((id, x, y, tipo, peptidos))
-            crearNodo(lista_adyacencias, id)
-            if tipo == 1:
-                crearArcos(lista_adyacencias, "i", id, float('inf'))
-            elif tipo == 2:
-                calculadoras.append(id)
-            elif tipo == 3:
-                crearArcos(lista_adyacencias, id, "f", float('inf'))
-            
-            for id1, x1, y1, tipo1, peptidos1 in celulas[:-1]:
-                capacidad = calcularCapacidad(peptidos, peptidos1)
+        try :
+            numCelulas, dist = linea.split()
+            numCelulas, dist = int(numCelulas), int(dist)
+            celulas = []
+            crearNodo(lista_adyacencias, "i")
+            crearNodo(lista_adyacencias, "f")
+            for i in range(numCelulas):
+                linea = sys.stdin.readline()
+                linea = linea.split()
+                id = (linea[0])
+                x = int(linea[1])
+                y = int(linea[2])
+                tipo = int(linea[3])
+                peptidos = linea[4:]
+                celulas.append((id, x, y, tipo, peptidos))
+                crearNodo(lista_adyacencias, id)
+                if tipo == 1:
+                    crearArcos(lista_adyacencias, "i", id, float('inf'))
+                elif tipo == 2:
+                    calculadoras.append(id)
+                elif tipo == 3:
+                    crearArcos(lista_adyacencias, id, "f", float('inf'))
+                
+                for id1, x1, y1, tipo1, peptidos1 in celulas[:-1]:
+                    capacidad = calcularCapacidad(peptidos, peptidos1)
 
-                if calcular_distancia_manhattan(x1, y1, x, y) <= dist and capacidad>0:
-                    if (tipo == 1 and tipo1 == 2):
-                        crearArcos(lista_adyacencias, id, id1, capacidad)
-                    elif (tipo == 2 and tipo1 == 1):
-                        crearArcos(lista_adyacencias, id1, id, capacidad)
-                        crearArcos(lista_adyacencias, id, id1, capacidad)
-                    elif (tipo == 2 and tipo1 == 3):
-                        crearArcos(lista_adyacencias, id, id1, capacidad)
-                    elif (tipo == 3 and tipo1 == 2):
-                        crearArcos(lista_adyacencias, id1, id, capacidad)
-                        crearArcos(lista_adyacencias, id, id1, capacidad)
-                    elif (tipo == 2 and tipo1 == 2):
-                        crearArcos(lista_adyacencias, id, id1, capacidad)
-                        crearArcos(lista_adyacencias, id1, id, capacidad)                
-        
-        flujo_sin_celula_bloqueo = float('inf')
-        #lista_original = copy.deepcopy(lista_adyacencias)
-        nuevaLista = copy.deepcopy(lista_adyacencias)
-        #nuevaLista = lista_adyacencias
-        #print("LISTA_ADYACENCIAS", lista_adyacencias)
-        for id_calculadora in calculadoras:
-            #nuevaLista = lista_adyacencias.copy()
-            #nuevaLista = copy.deepcopy(lista_adyacencias)   #si esta costoso, se puede solo hacer una copia, ¿como?
-            original = nuevaLista[id_calculadora]
-            nuevaLista[id_calculadora] = []
-            #print("ID calculadora", id_calculadora)
-            #print("LISTA ANTES", nuevaLista)
-            flujo = edmonds_karp(nuevaLista, "i", "f")
-            #print("LISTA_ADYACENCIAS", lista_adyacencias)
-            #nuevaLista = lista_original.copy()
-            nuevaLista[id_calculadora] = original
-            nuevaLista = lista_adyacencias
-            #print("LISTA DESPU", nuevaLista)
-            if flujo < flujo_sin_celula_bloqueo:
-                id_celula_bloqueo = id_calculadora
-                flujo_sin_celula_bloqueo = flujo
-            if flujo_sin_celula_bloqueo == 0:
-                break
-        
-        flujo_total = edmonds_karp(lista_adyacencias, "i", "f")        
-        print(id_celula_bloqueo, flujo_total, flujo_sin_celula_bloqueo)
-        #print(flujo_total)
-        #except:
-        #    print("Error")
+                    if calcular_distancia_manhattan(x1, y1, x, y) <= dist and capacidad>0:
+                        if (tipo == 1 and tipo1 == 2):
+                            crearArcos(lista_adyacencias, id, id1, capacidad)
+                        elif (tipo == 2 and tipo1 == 1):
+                            crearArcos(lista_adyacencias, id1, id, capacidad)
+                            crearArcos(lista_adyacencias, id, id1, capacidad)
+                        elif (tipo == 2 and tipo1 == 3):
+                            crearArcos(lista_adyacencias, id, id1, capacidad)
+                        elif (tipo == 3 and tipo1 == 2):
+                            crearArcos(lista_adyacencias, id1, id, capacidad)
+                            crearArcos(lista_adyacencias, id, id1, capacidad)
+                        elif (tipo == 2 and tipo1 == 2):
+                            crearArcos(lista_adyacencias, id, id1, capacidad)
+                            crearArcos(lista_adyacencias, id1, id, capacidad)                
+            
+            flujo_sin_celula_bloqueo = float('inf')
+            nuevaLista = copy.deepcopy(lista_adyacencias)
+            for id_calculadora in calculadoras:
+                original = nuevaLista[id_calculadora]
+                nuevaLista[id_calculadora] = []
+                flujo = edmonds_karp(nuevaLista, "i", "f")
+                nuevaLista[id_calculadora] = original
+                nuevaLista = lista_adyacencias
+                if flujo < flujo_sin_celula_bloqueo:
+                    id_celula_bloqueo = id_calculadora
+                    flujo_sin_celula_bloqueo = flujo
+                if flujo_sin_celula_bloqueo == 0:
+                    break
+            
+            flujo_total = edmonds_karp(lista_adyacencias, "i", "f")        
+            print(id_celula_bloqueo, flujo_total, flujo_sin_celula_bloqueo)
+        except:
+            print("Error")
         lista_adyacencias = {}
         calculadoras = []
         linea = sys.stdin.readline()
